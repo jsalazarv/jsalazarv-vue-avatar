053f65290ba7d9cb700565e0e0b58857b796e77d
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2021-12-05 18:57:09 -0600

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import VAvatar from '@/v-avatar.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VAvatar
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <v-avatar />
+  </div>
+</template>
---
+++ src/v-avatar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="v-avatar">
+    Avatar
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default class VAvatar extends Vue {}
+</script>
+
+<style scoped></style>
